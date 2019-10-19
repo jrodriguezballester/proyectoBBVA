9b4c783d34ef862dd3c590096e6f8bd887d3547a
Jose Rodriguez <jrodriguezballester@gmail.com>
2019-10-19 23:16:29 +0200

    20px

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,9 +1,7 @@
 #!/usr/bin/env bash
 
-
-
-
 echo building.... en marcha
+
 rm -rf ./dist
 
 mkdir ./dist
@@ -13,3 +11,4 @@ cp -r ./src/* ./dist
 mkdir ./dist/assets/css
 # compilar scss y ts
 node-sass .\\dist\\assets\\scss\\main.scss > .\\dist\\assets\\css\\main.css
+echo built
